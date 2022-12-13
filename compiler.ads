d948ff7c9eb30c4174cfe4b584fbf02fd6f2ccc9
Jay Patel <jaypatelani@gmail.com>
2022-12-13 13:47:17 +0530

    Create compiler.ads

---
+++ compiler.ads
@@ -0,0 +1,26 @@
+package Compiler is
+
+   -- Include the Front_End and Back_End units
+   with Compiler.Front_End;
+   with Compiler.Back_End;
+
+   -- Type definitions
+   type String is new Ada.Strings.Unbounded.Unbounded_String;
+   type File_Type is private;
+   type Declarations is private;
+   type Object_Code is private;
+
+   -- Parse the given source code and return the declarations
+   function Parse (Source : String) return Declarations;
+
+   -- Generate object code from the given declarations
+   function Generate_Code (Declarations : Declarations) return Object_Code;
+
+private
+
+   -- Type definitions for private elements
+   type File_Type is new Ada.Text_IO.File_Type;
+   type Declarations is array of Integer;
+   type Object_Code is array of Integer;
+
+end Compiler;
